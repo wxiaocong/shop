34193a211650980c5a8603ea00971019ba5ef517
xiaocong <441549088@qq.com>
2018-09-29 20:32:09 +0800

    16,19段手机

--- resources/views/users/editAddress.blade.php
+++ resources/views/users/editAddress.blade.php
@@ -164,7 +164,7 @@ $('.address-submit').click(function(){
     });
 });
 function isPoneAvailable(str) {
-    var myreg=/^[1][3,4,5,7,8][0-9]{9}$/;
+    var myreg=/^[1][3,4,5,6,7,8,9][0-9]{9}$/;
     if (!myreg.test(str)) {
         return false;
     } else {
